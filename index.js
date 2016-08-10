0bbf5280107d2e07d8cdb794b325360b91f1e6d3
derek_robertson <dwilliamrobertson@gmail.com>
2016-08-10 09:19:55 -0700

    set up db

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/interviewDb');
+mongoose.connect('mongodb://localhost/interview');
 
 var PORT = process.env.PORT || 3000;
 
